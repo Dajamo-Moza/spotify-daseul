a4e05d4c4433d828b47f341704c6892cfaadcbbb
daseuls <dls2tmfs2@naver.com>
2022-04-18 22:52:10 +0900

    feat:new release album api 생성

--- utils/APIcontrols.js
+++ utils/APIcontrols.js
@@ -1,3 +1,11 @@
-export const getRecentReleaseAlbum = () => {};
+import { getData } from "./fetchData";
+
+export const getRecentReleaseAlbums = () =>
+  getData("get", "/browse/new-releases", {
+    params: {
+      country: "KR",
+      limit: 20,
+    },
+  });
 
 export const getAlbumTracks = () => {};
