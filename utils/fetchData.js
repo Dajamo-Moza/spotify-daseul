8f1dc3422fde30e068bc17444949316d43a8d98a
daseuls <dls2tmfs2@naver.com>
2022-04-18 22:47:42 +0900

    feat: api key 추가

--- utils/fetchData.js
+++ utils/fetchData.js
@@ -3,6 +3,9 @@ import axios from "axios";
 export const getData = async (method, url, config) => {
   const instance = axios.create({
     baseURL: "https://api.themoviedb.org/3",
+    headers: {
+      Authorization: `Bearer ${process.env.API_KEY}`,
+    },
   });
   try {
     const res = await instance[method](url, config);
